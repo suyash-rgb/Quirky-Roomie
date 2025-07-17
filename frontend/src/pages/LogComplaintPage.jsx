ea868c5292b6afd7f4382d967465abae1fb066a8
Suyash Baoney <suyashbaoney58@gmail.com>
2025-07-18 04:51:20 +0530

    project cleanup

--- frontend/src/pages/LogComplaintPage.jsx
+++
@@ -1,9 +0,0 @@
-import ComplaintForm from "../components/Complaint Form/ComplaintForm";
-
-const LogComplaintPage = () => (
-  <main className="min-h-screen bg-gray-50 py-10">
-    <ComplaintForm />
-  </main>
-);
-
-export default LogComplaintPage;
